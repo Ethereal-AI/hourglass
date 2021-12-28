d22764e9ff9e741ee37b37f00f6a4500e9eb21b0
Grandmaster <grandmaster@ethereal-ai.com>
2021-12-28 17:11:06 +0800

    fix: Modify batch processing of tag

--- hourglass/tagger.py
+++ hourglass/tagger.py
@@ -39,19 +39,21 @@ class DateTimeTagger():
 		self.dummy_present = dummy_present
 		self.rules = load_rules()
 
-	def fetch_all_datetime_objects(self, datetime_entities):
+	def fetch_all_datetime_objects(self, datetime_entities: Union[Tuple, List]) -> Union[List, datetime]:
+		datetime_objects = list()
 		if self.dummy_present != None:
 			if isinstance(datetime_entities, Tuple):
 				datetime_objects = get_datetime_object(datetime_entities[0], self.dummy_present, self.rules)
 			elif isinstance(datetime_entities, List):
 				for substructure in datetime_entities:
 					if isinstance(substructure, List):
-						datetime_objects = fetch_all_datetime_objects(substructure)
+						datetime_objects.append(fetch_all_datetime_objects(substructure))
 					else:
-						datetime_objects = get_datetime_object(datetime_entities[0], self.dummy_present, self.rules)
+						print(substructure)
+						datetime_objects.append(get_datetime_object(substructure[0], self.dummy_present, self.rules))
 			return datetime_objects
 
-	def tag(self, texts: Union[List, str]) -> Union[Dict, List]:
+	def tag(self, texts: Union[List, str]) -> Union[List, datetime]:
 		"""
 		Tags the input text or texts via NER and rule rules.
 
@@ -65,6 +67,7 @@ class DateTimeTagger():
 			datetime_entities = detector.get_datetime_entities(texts)
 		elif isinstance(texts, List) and len(texts) > 1:
 			datetime_entities = list(map(lambda text: [self.tag(text)], texts))
+		print(datetime_entities)
 		datetime_objects = self.fetch_all_datetime_objects(datetime_entities)
 		return datetime_objects
 

e807cfb55eab28caded2ff35b2cd52dcb392ea77
Grandmaster <grandmaster@ethereal-ai.com>
2021-12-28 17:52:34 +0800

    chore: Split UNITS to plural and singular tuples

--- hourglass/utilities/rule_helper_functions.py
+++ hourglass/utilities/rule_helper_functions.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-UNITS = (
+UNITS_PLURAL = (
     "microseconds",
     "seconds",
     "minutes",
@@ -25,6 +25,10 @@ UNITS = (
     "years",
     "decades",
     "centuries",
+)
+
+
+UNITS_SINGULAR = (
     "microsecond",
     "second",
     "minute",
